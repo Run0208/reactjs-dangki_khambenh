006aa159d427b7fbea5e9429597250f7d5d0ce2a
Duong Dang Duc <duongdangduc02082000@gmail.com>
2021-08-31 00:45:13 +0700

    second commit login success!!!

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -5,7 +5,8 @@ import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 
 import './Login.scss';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
+import handleLoginApi from '../../services/userService';
 
 class Login extends Component {
     constructor(props) {
@@ -13,7 +14,8 @@ class Login extends Component {
         this.state = {
             username: '',
             password: '',
-            isShowPassword: false
+            isShowPassword: false,
+            errMessage: ''
         }
     }
 
@@ -27,10 +29,29 @@ class Login extends Component {
             password: event.target.value
         });
     }
-    hendleLogin = () => {
-        console.log('Username: ', this.state.username, 'Password: ', this.state.password);
-        console.log('All state', this.state);
-
+    handleLogin = async () => {
+        this.setState({
+            errMessage: ''
+        });
+        try {
+            let data = await handleLoginApi(this.state.username, this.state.password);
+            if(data && data.errCode !== 0) {
+                this.setState({
+                    errMessage: data.message
+                });
+            }
+            else if(data && data.errCode === 0) {
+                this.props.userLoginSuccess(data.user);
+                console.log('login success');
+            }
+        } catch(error) {
+            if(error.response && error.response.data) {
+                this.setState({
+                    errMessage: error.response.data.message
+                })
+            }
+            console.log('errro: ', error.response);
+        }
     }
 
     handleShowHidePassword = () => {
@@ -68,13 +89,15 @@ class Login extends Component {
                                 onChange={(event) => { this.handleOnChangePassword(event) }}
                                 />
                                 <span onClick={() => { this.handleShowHidePassword() }}>
-                                    <i class={this.state.isShowPassword ? 'fas fa-eye' : 'fas fa-eye-slash'}></i>
+                                    <i className={this.state.isShowPassword ? 'fas fa-eye' : 'fas fa-eye-slash'}></i>
                                 </span>
                             </div>
                         </div>
-                        
+                        <div className="col-12" style={{color: 'red', fontSize: "1rem"}}>
+                            {this.state.errMessage}
+                        </div>
                         <div className="col-12">
-                            <button className="btn-login" onClick={() => { this.hendleLogin() }}>Login</button>
+                            <button className="btn-login" onClick={() => { this.handleLogin() }}>Login</button>
                         </div>
                         <div className="col-12">
                             <span className="login-forgot-password">Forgot your password?</span>
@@ -107,8 +130,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         navigate: (path) => dispatch(push(path)),
-        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
-        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+        // userLoginFail: () => dispatch(actions.userLoginFail()),
+        userLoginSuccess: (userInfo) => dispatch(actions.userLoginSuccess(userInfo))
     };
 };
 
