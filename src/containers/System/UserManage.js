4f141089d7a45b24bf5eda1daa940262fc17764f
Duong Dang Duc <duongdangduc02082000@gmail.com>
2021-08-31 16:16:15 +0700

    four commit #38 css table-user

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
 // import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+
+import { getAllUsers } from '../../services/userService';
+
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('All');
+        if(response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            });
+        }
     }
 
 
     render() {
+        console.log('check render', this.state);
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <h1 className="title-user">TABLE USERS</h1>
+                <div className="users-table">
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                            {
+                                arrUsers && arrUsers.map((item, index) => {
+                                    return(
+                                        <tr>
+                                            <td>{item.email}</td>
+                                            <td>{item.firstName}</td>
+                                            <td>{item.lastName}</td>
+                                            <td>{item.address}</td>
+                                            <td>
+                                                <button className="btn-edit"><i class="fas fa-pencil-alt"></i></button>
+                                                <button className="btn-delete"><i class="fas fa-trash-alt"></i></button>
+                                            </td>
+                                        </tr>
+                                        
+                                    );
+                                })
+                            }
+                    </table>
+                </div>
+            </div>
         );
     }
 
